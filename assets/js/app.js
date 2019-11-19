44d77cd1e1589db258f7756b9f4181c39d5ffc9a
unknown <nikoladimitrijevic1991@gmail.com>
2019-11-19 21:16:59 +0100

    get started form

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,31 @@
 var divInsideButton = document.getElementsByClassName('divInsideButton')[0];
 var watchNowButton = document.getElementsByClassName('watchNowButton')[0];
 var getStartedNow = document.getElementsByClassName('getStartedNow');
+var getStartedNowParagraph = document.getElementsByClassName('getStartedNowParagraph');
+var closeGetStartedButtonInside = document.getElementsByClassName('closeGetStartedButtonInside');
+var sectionTeamsWeHelped = document.getElementsByClassName('sectionTeamsWeHelped');
 
 // get started now morph window
 getStartedNow[0].addEventListener('click',()=>{
-  getStartedNow[0].style.width = '50vw';
+  getStartedNow[0].style.width = '100%';
+  getStartedNow[0].style.background = 'white';
+  getStartedNowParagraph[0].style.color = 'white';
 });
 
-getStartedNow[0].addEventListener('mouseleave',()=>{
-  getStartedNow[0].style.width = '5.613vw';
+// getStartedNow[0].addEventListener('mouseleave',()=>{
+//   closeGetStartedFun();
+// });
+
+closeGetStartedButtonInside[0].addEventListener('click',(e)=>{
+    e.stopPropagation();
+    closeGetStartedFun();
 });
+
+function closeGetStartedFun() {
+  getStartedNow[0].style.background = 'rgba(255,255,255,0.4)';
+  getStartedNow[0].style.width = '5.613vw';
+  getStartedNowParagraph[0].style.color = 'rgb(255,213,51,1)';
+}
 
 // watch no button
 watchNowButton.addEventListener('mouseenter',function(){
@@ -82,6 +98,19 @@ setInterval(function () {
 }, 6000);
 
 
-window.onload = function(){
-
+// arrow to top
+var arrowToTop = document.getElementsByClassName('arrowToTop');
+function arrowToTopFun() {
+  if(sectionTeamsWeHelped[0].getBoundingClientRect().top - window.innerHeight /1.2 <= 0 ){
+    arrowToTop[0].style.opacity = '0.31'
+    arrowToTop[0].style.pointerEvents = 'auto'
+  }else{
+    arrowToTop[0].style.opacity = '0'
+    arrowToTop[0].style.pointerEvents = 'none'
+  }
 }
+
+
+window.addEventListener('scroll',()=>{
+  arrowToTopFun();
+})
